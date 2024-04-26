bb0d32a37a432eeba53df6c046d3bf3377500de3
fernando <fediniz41@gmail.com>
2024-04-26 06:22:16 -0300

    calc.rb

---
+++ Hello.rb
@@ -0,0 +1,11 @@
+print 'Digite seu nome: '
+
+name = gets.chomp
+
+
+print 'Digite seu sobrenome: '
+
+sobrename = gets.chomp
+
+
+puts "Hello #{name} #{sobrename}!"
---
+++ calc.rb
@@ -0,0 +1,17 @@
+print "Digite o primeiro numero inteiro: "
+number1 = gets.chomp.to_i
+
+print "Digite o segundo numero inteiro: "
+number2 = gets.chomp.to_i
+addition = number1 + number2
+subtraction = number1 - number2
+multiplication = number1 * number2
+division = number1 / number2
+puts "O resultado da adição entre os dois numeros é #{addition}"
+puts "O resultado da subtração entre os dois numeros é #{subtraction}"
+puts "O resultado da multiplicação entre os dois numeros é #{multiplication}"
+puts "O resultado da divisão entre os dois numeros é #{division}"
+
+
+
+
